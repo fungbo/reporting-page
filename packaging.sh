fbcde855d2fa66707c01c4935b4004247d4c9521
bfeng <fungbo@126.com>
2016-09-28 18:05:07 +0800

    Bo - Update the script

--- packaging.sh
+++ packaging.sh
@@ -5,7 +5,15 @@ webpack
 
 echo "remove config"
 sed -i -e "s/headers: {'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q='}//g" ./scripts/utils/cal-url.js
-rm ./scripts/utils/cal-url.js-e
+
+echo "copy js and css"
+cp ./build/* .
 
 echo "packaging"
-zip -r reporting-page.zip build index.html manifest.webapp icon.png
+zip -r reporting-page.zip index.html bundle.js vendor.bundle.js bundle.css manifest.webapp icon.png
+
+echo "remove temporary files"
+rm ./scripts/utils/cal-url.js-e
+rm ./bundle.js
+rm ./vendor.bundle.js
+rm ./bundle.css
