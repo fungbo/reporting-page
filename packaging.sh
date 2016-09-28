8e0ed418366fcafbcbbb1f14a30187683763d431
bfeng <fungbo@126.com>
2016-09-28 18:09:52 +0800

    Bo - Use regexp to search config

--- packaging.sh
+++ packaging.sh
@@ -4,7 +4,7 @@ echo "build project"
 webpack
 
 echo "remove config"
-sed -i -e "s/headers: {'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q='}//g" ./scripts/utils/cal-url.js
+sed -i -e "s/headers: {'Authorization': 'Basic .*'}//g" ./scripts/utils/cal-url.js
 
 echo "copy js and css"
 cp ./build/* .
